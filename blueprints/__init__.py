6499f834fd621c142bc5cb3c64e5fe7c21b79893
Hans Doller <hans.doller@coorworks.com>
2024-12-05 03:55:40 +0000

    More updates.

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -1,16 +1,16 @@
 """Blueprint initializer module. Loads all active blueprint modules."""
 
-import os
-from flask import Blueprint, request, send_file, render_template
+from flask import Blueprint, request, send_file
 from . import index, kernel
-from ..extensions import auth
 bp = Blueprint("app", __name__)
 
 @bp.route("/favicon.ico", methods=['GET'])
 def favicon():
+	"""Endpoint to handle favicon.ico image"""
 	return send_file('images/favicon.ico')
 
 def init_app(app):
+	"""Method to initialize all application modules."""
 	app.register_blueprint(bp)
 	app.register_blueprint(index.bp)
 	app.register_blueprint(kernel.bp)
@@ -18,8 +18,7 @@ def init_app(app):
 	@app.context_processor
 	def globalvariables():
 		blueprint, method = request.url_rule.endpoint.split(sep='.', maxsplit=2)
-
-		return dict(
-			__blueprint__=blueprint,
-			__method__=method
-		)
+		return {
+			'__blueprint__': blueprint,
+			'__method__': method
+		}
