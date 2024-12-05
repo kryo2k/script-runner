a815ace988dbf17f9fa6c83771515ed97455319d
Hans Doller <hans.doller@coorworks.com>
2024-12-05 04:29:00 +0000

    More linting.

--- extensions.py
+++ extensions.py
@@ -1,17 +1,22 @@
+"""All extensions required for application functionality."""
+
 import os
 from flask import render_template
 from flask_httpauth import HTTPBasicAuth
 from flask_socketio import SocketIO
 
 class SimpleAuthFile:
+	"""Object representing a file used for authentication checks."""
 	def __init__(self, path):
 		self._path = path
 	@property
 	def path(self):
+		"""Returns configured path of file."""
 		return self._path
 	def authenticate(self, username, password):
+		"""Attempts to authenticate username and password against file."""
 		try:
-			with open(self._path, 'r') as f:
+			with open(self._path, 'r', encoding="utf-8") as f:
 				for line in f:
 					stored_username, stored_password = line.strip().split(':')
 					if stored_username == username and stored_password == password:
@@ -28,10 +33,12 @@ socketio = SocketIO(cors_allowed_origins='*')
 
 @auth.verify_password
 def verify_password(username, password):
+	"""Method  to check a username and password for validity."""
 	if not auth_enabled or auth_file.authenticate(username, password):
 		return (username, password)
 	return None
 
 @auth.error_handler
 def unauthorized():
+	"""Endpoint to send users which are unauthorized to."""
 	return render_template('unauthorized.html')
