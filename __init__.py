8d94110f3b02ca7fcc33969605fb7a024dd10cca
Hans Doller <hans.doller@coorworks.com>
2024-12-05 03:45:41 +0000

    More changes to get pylint to shut up.

--- __init__.py
+++ __init__.py
@@ -19,64 +19,50 @@ kernel.init_app(app)
 socketio.init_app(app)
 
 @app.template_filter()
-"""
-Jinja2 template function to format datetime.datetime objects.
-"""
 def fmtdatetime(value, format='medium'):
+	"""Jinja2 template function to format datetime.datetime objects."""
 	if not isinstance(value, datetime):
 		return '--'
 	return format_datetime(value.astimezone(tz), format, locale=locale)
 
 @app.template_filter()
-"""
-Jinja2 template function to format datetime.date objects.
-"""
 def fmtdate(value, format='medium'):
+	"""Jinja2 template function to format datetime.date objects."""
 	if not isinstance(value, date):
 		return '--'
 	return format_date(value, format, locale=locale)
 
 @app.template_filter()
-"""
-Jinja2 template function to format datetime.time objects.
-"""
 def fmttime(value, format='medium'):
+	"""Jinja2 template function to format datetime.time objects."""
 	if not isinstance(value, time):
 		return '--'
 	return format_time(value, format, locale=locale)
 
 @app.template_filter()
-"""
-Jinja2 template function to format boolean objects.
-"""
 def fmtbool(value):
+	"""Jinja2 template function to format boolean objects."""
 	if not isinstance(value, bool):
 		return '--'
 	return 'Yes' if value else 'No'
 
 @app.template_filter()
-"""
-Jinja2 template function to format integer objects.
-"""
 def fmtint(value):
+	"""Jinja2 template function to format integer objects."""
 	if not isinstance(value, int):
 		return '--'
 	return format_number(value)
 
 @app.template_filter()
-"""
-Jinja2 template function to format float objects.
-"""
 def fmtfloat(value):
+	"""Jinja2 template function to format float objects."""
 	if not isinstance(value, float):
 		return '--'
 	return format_decimal(value)
 
 @app.template_filter()
-"""
-Jinja2 template function to format string objects.
-"""
 def fmtstr(value):
+	"""Jinja2 template function to format string objects."""
 	if not isinstance(value, str):
 		return '--'
 	return value
